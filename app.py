e96b3741ac4334578f6854ada509382b756386e3
Jason R. Coombs <jason.coombs@yougov.com>
2019-03-25 16:17:20 -0400

    Simplify behavior by removing authorization code and relying entirely on access token from client.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 __requires__ = ['flask_bootstrap', 'flask', 'requests_toolbelt']
 
-import urllib.parse
 import logging
 import os
 
@@ -14,11 +13,9 @@ app.secret_key = "Secret key"
 Bootstrap(app)
 
 FACEBOOK_APP_ID = os.environ['FACEBOOK_APP_ID']
-FACEBOOK_APP_SECRET = os.environ['FACEBOOK_APP_SECRET']
-FACEBOOK_GRAPH_VERSION = 'v3.2'
+FACEBOOK_APP_SECRET = os.environ.get('FACEBOOK_APP_SECRET')
+FACEBOOK_GRAPH_VERSION = os.environ.get('FACEBOOK_GRAPH_VERSION', 'v3.2')
 FACEBOOK_SCOPE = 'user_posts'
-
-TOKENS = {}
 
 facebook = sessions.BaseUrlSession(
     f'https://graph.facebook.com/{FACEBOOK_GRAPH_VERSION}/')
@@ -29,55 +26,6 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S')
 
 
-class NotAuthorizedException(Exception):
-    pass
-
-
-# OAuth functions
-
-
-def get_app_token():
-    """
-    Get an app token based on app ID and secret
-
-    :return:
-    """
-    params = dict(
-        client_id=FACEBOOK_APP_ID,
-        client_secret=FACEBOOK_APP_SECRET,
-        grant_type='client_credentials',
-    )
-    try:
-        resp = facebook.get('/oauth/access_token', params=params)
-        resp.raise_for_status()
-        return resp.json()["access_token"]
-    except KeyError:
-        logging.log(logging.ERROR, resp.text)
-        raise NotAuthorizedException(
-            "Authorization error", "App access token not found")
-
-
-def get_user_token(code):
-    params = dict(
-        client_id=FACEBOOK_APP_ID,
-        redirect_uri=callback_url(),
-        client_secret=FACEBOOK_APP_SECRET,
-        code=code,
-    )
-    try:
-        resp = facebook.get(
-            f'oauth/access_token',
-            params=params,
-        )
-        return resp.json()["access_token"]
-    except KeyError:
-        logging.log(logging.ERROR, resp.text)
-        raise NotAuthorizedException(
-            "Authorization error", "User access token not found")
-
-# App routes
-
-
 @app.route("/")
 def serve_home():
     """
@@ -86,60 +34,15 @@ def serve_home():
     :return: Renders the home page template
     """
 
-    # Check whether the user has authorized the app,
-    # if authorized login button will not be displayed
-    user_authorized = "user_token" in TOKENS
-
-    return flask.render_template(
-        "index.html",
-        authorized=user_authorized,
-        FACEBOOK_APP_ID=FACEBOOK_APP_ID,
-        FACEBOOK_GRAPH_VERSION=FACEBOOK_GRAPH_VERSION,
-        FACEBOOK_SCOPE=FACEBOOK_SCOPE,
-    )
-
-
-@app.route("/authorize")
-def authorize_facebook():
-    """
-    Redirect the user to the Facebook login page to authorize the app
-    """
-    qs = urllib.parse.urlencode(dict(
-        client_id=FACEBOOK_APP_ID,
-        redirect_uri=callback_url(),
-        scope='publish_actions',
-    ))
-    url = 'https://www.facebook.com/dialog/oauth?' + qs
-    return flask.redirect(url)
-
-
-@app.route("/callback")
-def handle_callback():
-    """
-    Handles callback after user authorization of app,
-    calling back to exchange code for access token
-
-    :return:
-    """
-    try:
-        TOKENS["user_token"] = get_user_token(flask.request.args.get("code"))
-
-        return flask.redirect("/")
-    except NotAuthorizedException:
-        return 'Access was not granted or authorization failed', 403
-
-
-def callback_url():
-    return urllib.parse.urljoin(flask.request.base_url, '/callback')
+    return flask.render_template("index.html", **globals())
 
 
 @app.route("/posts")
 def posts():
-    # Make sure there is a token
-    try:
-        token = TOKENS["user_token"]
-    except KeyError:
-        return 'Not authorized', 401
+    """
+    Retrieve the posts for the user
+    """
+    token = flask.request.args['access_token']
 
     resp = facebook.get(
         f'me/feed',
@@ -147,11 +50,8 @@ def posts():
     )
     if not resp.ok:
         return f'Unexpected resp from Facebook: {resp}'
-    return resp.text
+    return flask.jsonify(resp.json()['data'])
 
 
 if __name__ == '__main__':
-    # Register an app token at start-up (purely as
-    # validation that configuration for Facebook is correct)
-    TOKENS["app_token"] = get_app_token()
     app.run(host='::', port=int(os.environ.get('PORT', 8080)), debug=True)
--- static/index.js
+++ static/index.js
@@ -1,11 +1,23 @@
+function screen_log(message) {
+  document.querySelector('#content').innerHTML += message + '<br />'
+}
+
+
+function handle_posts(posts) {
+  screen_log("Found " + posts.length + " posts")
+}
+
 window.fbAsyncInit = function() {
   FB.init(facebook_options);
   FB.login(function(response) {
     if (response.authResponse) {
-      console.log('Welcome!  Fetching your information.... ');
+      screen_log('Welcome!  Fetching your information.... ');
       FB.api('/me', function(response) {
-        console.log('Good to see you, ' + response.name + '.');
+        screen_log('Good to see you, ' + response.name + '.');
       });
+      fetch('/posts?access_token=' + response.authResponse.accessToken)
+        .then(response => response.json())
+        .then(handle_posts)
     } else {
       console.log('User cancelled login or did not fully authorize.');
     }
